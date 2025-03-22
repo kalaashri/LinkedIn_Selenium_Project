80fe7fe7949221ac0fe331c755cf4fb798c7d1ce
balachandarlb <balachandarlb@gmail.com>
2025-03-22 11:38:57 +0100

    BasePage changes

--- src/main/java/com/LinkedIn/qa/Pages/HomePage.java
+++ src/main/java/com/LinkedIn/qa/Pages/HomePage.java
@@ -1,4 +1,20 @@
 package com.LinkedIn.qa.Pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.LinkedIn.qa.base.BasePage;
+
+public class HomePage extends BasePage{
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    By logo=By.xpath("//a[@aria-label='LinkedIn']");
+
+    public HomePage logo(String logoText){
+        setText(logo, logoText);
+        return this;
+    }
 }
